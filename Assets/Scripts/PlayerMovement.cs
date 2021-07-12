dd2b50e10e6de4491a5572dd4e6bbe85437ecff9
Maddy <scii.maddy@gmail.com>
2021-07-13 01:38:08 +0200

    Drew map, made an almost perfect moving script for player in new Unity Input System just to find out it is not compatible with multiple players on one keyboard. The game can look off on different resolutions.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,24 +3,57 @@ using System.Collections.Generic;
 //using System.Numerics;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Rigidbody2D rb;
+    //private PlayerInputScript PlayerInput;
+    Rigidbody2D rb;
     public float speed;
     public float jump = 20f;
     float movementX;
     public Transform feet;
     public LayerMask groundLayers;
 
+    
+    void Start()
+    {
+        //PlayerInput = new PlayerInputScript();
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void OnEnable()
+    {
+        //PlayerInput.Enable();
+    }
+    void OnDisable()
+    {
+        //PlayerInput.Disable();
+    }
     // Update is called once per frame
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        movementX = context.ReadValue<float>();
+    }
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.action.triggered)
+        {
+            Jump();
+        }
+    }
     void Update()
     {
+        //movementX = PlayerInput.Basic.Movement.ReadValue<float>();
+        //Vector3 currentPosition = transform.position;
+        //currentPosition.x += movementX * speed * Time.deltaTime;
+        //transform.position = currentPosition;
+        /*
         movementX = Input.GetAxisRaw("Horizontal");
         if (Input.GetButtonDown("Jump") && TouchesGround())
         {
             Jump();
-        }
+        }*/
     }
     void FixedUpdate()
     {
@@ -29,8 +62,13 @@ public class PlayerMovement : MonoBehaviour
     }
     void Jump()
     {
-        Vector2 movement = new Vector2(rb.velocity.x, jump);
-        rb.velocity = movement;
+        if (TouchesGround())
+        {
+            //Vector2 movement = new Vector2(rb.velocity.x, jump);
+            //rb.velocity = movement;
+            rb.AddForce(new Vector2(0, jump), ForceMode2D.Impulse);
+        }
+        
     }
     bool TouchesGround()
     {
