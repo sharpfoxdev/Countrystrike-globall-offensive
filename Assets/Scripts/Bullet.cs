790e782db89cbb48f07dfaec4e07b2a39bd0dc07
Maddy <scii.maddy@gmail.com>
2021-07-17 01:01:36 +0200

    can change skins in unity now and made AK47 shoot faster bullets

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,7 +5,7 @@ using UnityEngine.InputSystem;
 
 public class Bullet : MonoBehaviour
 {
-    public float speed = 20f;
+    public float speed = 20;
     public Rigidbody2D rb;
     //public int SpawnDelay = 1;
     //public GameObject playerManager;
@@ -13,6 +13,7 @@ public class Bullet : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        Debug.Log(speed);
         rb.velocity = transform.right * speed;
     }
     void OnTriggerEnter2D(Collider2D hitInfo)
--- Assets/ak47.cs
+++ Assets/ak47.cs
@@ -11,8 +11,11 @@ public class ak47 : MonoBehaviour
     bool isShooting;
     public void Shoot()
     {
+        float speedBefore = BulletPrefab.GetComponent<Bullet>().speed;
+        BulletPrefab.GetComponent<Bullet>().speed = 45f; // this is changing the prefab!!!
+        Debug.Log(BulletPrefab.GetComponent<Bullet>().speed);
         Instantiate(BulletPrefab, FirePoint.position, FirePoint.rotation);
-
+        BulletPrefab.GetComponent<Bullet>().speed = speedBefore;
 
     }
     IEnumerator ShootOneBullet(int depth, int currentDepth)
@@ -22,6 +25,7 @@ public class ak47 : MonoBehaviour
             
         }
         yield return new WaitForSeconds(1f);
+        
         Instantiate(BulletPrefab, FirePoint.position, FirePoint.rotation);
         StartCoroutine(ShootOneBullet(depth, currentDepth++));
     }
