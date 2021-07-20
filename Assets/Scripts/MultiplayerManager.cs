591f007b43fa5566fb9d8e2afbe8be8105d37510
Maddy <scii.maddy@gmail.com>
2021-07-20 14:33:49 +0200

    better main menu

--- Assets/Scripts/MultiplayerManager.cs
+++ Assets/Scripts/MultiplayerManager.cs
@@ -33,7 +33,6 @@ public class MultiplayerManager : MonoBehaviour
                 Destroy(player);
             }
             MainMenu.SetActive(true);
-            //MainMenu.OnEnabled();
         }
     }
     public void DisappearPlayer(GameObject player)
@@ -61,7 +60,7 @@ public class MultiplayerManager : MonoBehaviour
             player.transform.position = new Vector2(Random.Range(rt.rect.xMin, rt.rect.xMax), Random.Range(rt.rect.yMin, rt.rect.yMax)); //random place in game area
                                                                                                                                          //player.transform.position = new Vector2(Random.Range(-15f, 15f), Random.Range(-8f, 8f)); 
             player.GetComponent<PlayerMovement>().FaceRight(); //making him face right, otherwise he can sometimes move in the oposite direction than he is facing after respawning
-
+            
             PlayerInput.Instantiate(player, controlScheme: ctrlscheme, pairWithDevice: Keyboard.current);
             Destroy(player); //creating copy and deleting disfunctional original
         }
