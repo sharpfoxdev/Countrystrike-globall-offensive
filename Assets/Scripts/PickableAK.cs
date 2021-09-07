14a0d416ef25abe77406b09e66dd5892e0396ba9
Maddy <scii.maddy@gmail.com>
2021-09-07 20:15:55 +0200

    Second commit, something didnt get pushed

---
+++ Assets/Scripts/PickableAK.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+/// <summary>
+/// Script for managing pickable ak47
+/// </summary>
+public class PickableAK : MonoBehaviour
+{
+    int enteredRigidbodiesInTrigger = 0; //tracks, what everithing is in the collider box
+
+    [SerializeField]
+    GameObject gunSpawner; //TODO
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        enteredRigidbodiesInTrigger++; //so that it doesn't spam map in a case there is more things inside the trigger
+        if (enteredRigidbodiesInTrigger > 1)
+        {
+            return;
+        }
+        Debug.Log(hitInfo.name);
+        Player player = hitInfo.GetComponent<Player>();
+        if (player != null)
+        {
+            player.GetComponentInChildren<Weapon>().ReloadWeapon("AK47");
+        }
+        GameObject.FindGameObjectWithTag("Spawner").GetComponent<Spawner>().SpawnNextWeapon(); //TODO
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/WeaponsEnum.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum WeaponsEnum
+{
+    Glock,
+    AK47,
+    M58B
+}
